22fda1774dceb5958052fc9d4506169e281ebcdb
Lakshin Kumar <5782426-_lakshin@users.noreply.gitlab.com>
2021-03-26 19:59:30 +0530

    automated commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-
 import './App.css';
-
+import Header from './components/Layouts/Header';
+import Footer from './components/Layouts/Footer';
 function App() {
   return (
     <div className="App">
-      Hi.
+      <Header/>
+      <div className="">
+         Hi.
+      </div>
+
+      <Footer/>
     </div>
   );
 }
--- client/src/components/Layouts/Header.js
+++ client/src/components/Layouts/Header.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
-
+import "./Header.css"
 export default class Header extends Component {
   render() {
     return (
-      <>
-        
-      </>
+      <header>
+        Header
+      </header>
     )
   }
 }
