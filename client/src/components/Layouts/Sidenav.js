1f60c65a8b45ed647c36abbd7ba2c27bd6b6c2b8
SaI7Tejashwin <esaitejashwin@gmail.com>
2021-03-27 01:39:46 +0530

    automated commit

---
+++ client/src/components/Layouts/Sidenav.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import App from '../../App';
+
+const Sidenav = () => {
+    return (
+        <div className="sidenav">
+           <p> basic layout </p>
+        </div> 
+    );
+  };
+
+  export default Sidenav;
