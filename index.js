0194d50a18d8c789a9445976875a14da35410621
Lakshin Kumar <5782426-_lakshin@users.noreply.gitlab.com>
2021-03-27 16:54:33 +0530

    automated commit

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const app = require('./app');
 const mongoose = require('mongoose');
 const config = require('./config');
-
+const path=require("path");
 const connect = url => {
   return mongoose.connect(url, config.db.options);
 };
+
+if(process.env.NODE_ENV=== 'production'){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
+
+
 
 if (require.main === module) {
   app.listen(config.port);
