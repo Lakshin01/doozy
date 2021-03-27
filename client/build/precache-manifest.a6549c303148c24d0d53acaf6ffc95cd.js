0a8588d7ec4b0af88730657c82e963e4cd6ce3e3
Lakshin Kumar <5782426-_lakshin@users.noreply.gitlab.com>
2021-03-27 17:16:14 +0530

    automated commit

---
+++ client/build/precache-manifest.a6549c303148c24d0d53acaf6ffc95cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e772103efa8bd2b2a1d",
+    "url": "/static/css/main.25b02b6f.chunk.css"
+  },
+  {
+    "revision": "6e772103efa8bd2b2a1d",
+    "url": "/static/js/main.edda9305.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "08af61abe151cfb71f61",
+    "url": "/static/js/2.8e28d54f.chunk.js"
+  },
+  {
+    "revision": "b66bb57239af8679816bf69c0a846ac6",
+    "url": "/index.html"
+  }
+];
